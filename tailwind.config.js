483c260151f1d77da8bf89997ed5c66e62657ad0
Austin Wasson <austinpwasson@gmail.com>
2023-12-08 09:20:38 -0500

    init

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -7,10 +8,10 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        'heading': ['CabinetGrotesk', 'sans-serif'],
+        'body': ['Lato', ...defaultTheme.fontFamily.sans],
       },
     },
   },
